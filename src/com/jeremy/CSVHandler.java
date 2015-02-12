93e2a35405e06c1e85954e8baaf03950c492c6f0
FewCode <alex.brown45@tafensw.net.au>
2015-02-12 20:54:54 +1100

    Added more to the CSVHandler so i can try and commit properly

--- src/com/jeremy/CSVHandler.java
+++ src/com/jeremy/CSVHandler.java
@@ -1,5 +1,26 @@
 package com.jeremy;
 
+import java.io.File;
+
 public class CSVHandler {
 
+	public Object[][] readCSV(String fileName){
+		
+		
+		return new Object[12][12];
+	}
+	
+	public Object[][] readCSV(String directory, String fileName){
+		
+		
+		return new Object[12][12];
+	}
+	
+	
+	public Object[][] readCSV(File csvFile){
+		
+		
+		return new Object[12][12];
+	}
+	
 }
